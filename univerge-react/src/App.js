90232370bc771fa0f835c0b0da7faf23a1758bfe
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-03-03 16:23:52 -0600

    navigational pages

--- univerge-react/src/App.js
+++ univerge-react/src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import MentorSignUp from './components/pages/MentorSignUp';
 
-const App = () => <div> Hello world </div>
+const App = () => (
+<div>
+  <Route path="/" exact component={HomePage} />
+  <Route path="/mentorsignup" exact component={MentorSignUp} />
+</div>
+);
 
 export default App;
---
+++ univerge-react/src/components/pages/MentorSignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MentorSignUp = () => (
+  <div>
+    <h1> Mentor SignUp Page </h1>
+    <Link to='/'> Back Home </Link>
+  </div>
+);
+
+export default MentorSignUp;
