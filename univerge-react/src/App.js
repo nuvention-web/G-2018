9dcbe6f3463977020e948997146e699993e3e6ac
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-04-19 20:30:45 -0500

    adding new nice content

--- univerge-react/src/App.js
+++ univerge-react/src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
-import HomePage from './components/pages/HomePage';
-import MentorSignUp from './components/pages/MentorSignUp';
-import MenteeSignUp from './components/pages/MenteeSignUp';
+// import HomePage from './components/pages/HomePage';
+// import MentorSignUp from './components/pages/MentorSignUp';
+// import MenteeSignUp from './components/pages/MenteeSignUp';
+import LandingPage from './components/pages/LandingPage';
+import About from './components/pages/About';
+
+// export default class App extends Component {
+// render() {
+//     return (
+//       <div className='ui container fluid'>
+
+//         <Route path="/" exact component={HomePage} />
+//         <Route path="/mentorsignup" exact component={MentorSignUp} />
+//         <Route path="/menteesignup" exact component={MenteeSignUp} />
+//       </div>
+//     )
+//   }
+// }
+
 
 export default class App extends Component {
-render() {
-    return (
-      <div className='ui container fluid'>
-
-        <Route path="/" exact component={HomePage} />
-        <Route path="/mentorsignup" exact component={MentorSignUp} />
-        <Route path="/menteesignup" exact component={MenteeSignUp} />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Route path="/" exact component={LandingPage}/>
+				<Route path="/about" exact component={About}/>
+			</div>
+		)
+	}
 }
 
 // export default App;
