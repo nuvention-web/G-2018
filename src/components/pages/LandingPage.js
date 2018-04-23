f69beeceb63037f6fd5e7172ffc1686035a0fcea
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-04-23 15:17:15 -0500

    more components

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -5,45 +5,18 @@ import About from './About';
 import Mentor from './Mentor';
 import Mentee from './Mentee';
 import Team from './Team';
+import Navbar from './Navbar';
+import Team from './Title';
 
 
 const LandingPage = () => (
 
 
 	<div>
-    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i class="fa fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a>
-            </li>
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
-            </li>
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    
 
-    <header class="masthead bg-primary text-white text-center">
-      <div class="container">
-        <img class="img-fluid mb-5 d-block mx-auto" src="img/profile.png" alt="" /> 
-        <h1 class="text-uppercase mb-0">Start Bootstrap</h1>
-        <hr class="star-light" />
-        <h2 class="font-weight-light mb-0">Web Developer - Graphic Artist - User Experience Designer</h2>
-      </div>
-    </header>
-
-
+    <Navbar />
+    <Title />
     <About />
     <Mentor />
     <Mentee />
