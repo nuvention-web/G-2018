12877cd014c740bb95346f57ae8de1584af28775
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-04-23 22:10:12 -0500

    centered team images

--- src/components/pages/Team.js
+++ src/components/pages/Team.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
 
 const Team = () => (
   	<section class="mb-0" id="team">
       <div class="container">
         <h2 class="text-center text-uppercase">Team</h2>
         <br />
+
+        <div class="row">
+          <div class="col-md-6 col-lg-4">
+            <img class="img-fluid profpic" src={logo} alt="" />
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <img class="img-fluid profpic" src= {logo} alt="" />
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <img class="img-fluid profpic" src={logo} alt="" />
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <img class="img-fluid profpic" src={logo} alt="" />
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <img class="img-fluid profpic" src={logo} alt="" />
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <img class="img-fluid profpic" src={logo} alt="" />
+          </div>
+        </div>
+
       </div>
     </section>
 );
