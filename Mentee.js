6e46c555b3610df8285ec7c6a74a0bcaf2d3a82a
Charbel Bourjas <bourjasc@gmail.com>
2018-04-30 20:42:57 -0500

    Add files via upload
    mentor and mentee done

---
+++ Mentee.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Mentee = () => (
+  <section class="bg-primary text-white mb-0" id="mentee">
+    <div class="container">
+      <h2 class="text-center text-uppercase text-white">Mentee</h2>
+    <br />
+  <div class="row">
+  <div class="col-lg-5 ml-auto">
+  <p class="lead">High School:Will you be the first in your family to attend college? Are you full of aspirations for your future, but unsure of how to get from Point A to Point B? Do you see college as a launching pad to achieving your future goals? Are you curious, driven, and seeking resources beyond what your school offers? Your Kyte Triad will help you reach your goals and beyond.
+  </p>
+  </div>
+  <div class="col-lg-4 mr-auto">
+  <p class="lead">College student: Are you a first-generation college student pursuing an undergraduate or graduate degree? Are you a driven learner between the ages of 18 and 25 seeking comprehensive feedback and personal connections in the professional world? Are you willing to serve as a guide for a younger student, helping them navigate the college application process and sharing what you’ve learned along the way? Your mentor will help you achieve your career goals, and you will be giving back to the next generation.
+  </p>
+  </div>
+  </div>
+  </div>
+  </section>
+);
+
+export default Mentee;
