2c6a240d01873dc3608f4d4f6a67b7af9958f302
Charbel Bourjas <bourjasc@gmail.com>
2018-04-30 20:45:30 -0500

    mentee / mentor pages

--- src/components/pages/Mentor.js
+++ src/components/pages/Mentor.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Mentor = () => (
-  <div>
-    <h1> Mentor </h1>
-  </div>
+  <section class="mb-0" id="mentor">
+    <div class="container">
+      <h2 class="text-center text-uppercase">Mentor</h2>
+      <p>Are you a working professional under the age of 35 looking for a way to give back? Kyte mentors are knowledgeable and empathetic. They commit to working with two students for at least one year, designing projects and offering expertise and advice. What’s in in for you?
+You are investing in the future of our workforce! Strengthen your professional portfolio, expand your network, advance your charitable giving, and most importantly, help ensure that motivated young students have the opportunity to become changemakers like yourself!
+</p>
+      <br />
+      </div>
+  </section>
 );
 
 export default Mentor;
